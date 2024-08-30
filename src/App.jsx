96641ce929d35ff3d732f1a0ca5924182e0919c4
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-08-30 20:03:44 +0500

    upload

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Container from './Container.jsx';
+import Simple from './assets/Components/Simple.jsx';
 function App() {
 
 
   return (
-   <>
-   <Container/>
-   </>
+    <>
+   <Simple/>
+    </>
   )
 }
 
