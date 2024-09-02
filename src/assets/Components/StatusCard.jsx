3d8fb9497a1430e1039ce197fceae09bfd15b7cd
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-09-02 18:26:25 +0500

    change upload

--- src/assets/Components/StatusCard.jsx
+++ src/assets/Components/StatusCard.jsx
@@ -1,10 +1,10 @@
 import { useState, useEffect } from "react";
 
 function StatusCard() {
-    const [status, setStatus] =useState("");
-    const [text, setText] = useState("");
-    const [bg, setBg] = useState("");
-    const [textColor, setTextColor] = useState("");
+    const [status, setStatus] =useState([]);
+    const [text, setText] = useState("Hello");
+    const [bg, setBg] = useState("#00000");
+    const [textColor, setTextColor] = useState("#ffffff");
 
     const handleOnClick = () => {
         const obj = {
@@ -27,13 +27,16 @@ function StatusCard() {
                 <label>
                     Status Text
                     <input value={text} onChange={(e) => setText(e.target.value)}
-                    className={`w-60 p-1 border rounded-md mx-4`} type="color" placeholder="Select Bg COlor" />
+                    className={`w-60 p-1 border rounded-md mx-4`} type="text" placeholder="Add Text" />
                 </label>
-                <label>Text Color
-                    <input  value={textColor} 
-                    onClick={(e) => setTextColor(e.target.value)}
-                    className={`w-60 p-1 border rounded-md mx-4`}type="color" placeholder="Select Text Color" />
+                <label>Bg Color
+                    <input  value={bg} 
+                    onClick={(e) => setBg(e.target.value)}
+                    className={`w-60 p-1 border rounded-md mx-4`}type="color" placeholder="Select Bg Color" />
                 </label>
+                <label> Text Color
+                    <input value={textColor} onChange={(e) => setTextColor(e.target.value)} type="color" placeholder="Select Text Color" /> 
+                    </label>
                 <button onClick={handleOnClick} className="p-1 bg-slate-700 text-White px-3 rounded-md">
                     Add
                 </button>
